f51740c782c0e9453913e414509d1e8b15ff28b3
Hilko <hilko.j@wwu.de>
2017-06-07 21:39:53 +0200

    Starting with the configuration.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # TwitchPreviewCrawler
 Crawls Twitch.tv's (temporary) preview pictures of the stream.
 
+# Requirements
+This crawler was tested with python 3.6.1. Please use pip to download requirements (see next step, Installation).
+
 # Installation
 Use the requirements.txt to install all required packages
 
---
+++ TwitchPreviewCrawler.py
@@ -0,0 +1,44 @@
+# This is the main class for the crawler
+
+# Imports
+from twitch import TwitchClient
+import configparser
+import os
+
+class TwitchPreviewCrawler:
+
+	def __init__(self, twitch_api_key):
+		self.twitch_api_key = twitch_api_key
+		self.client = TwitchClient(client_id = self.twitch_api_key)
+
+
+def main():
+	# Load data from config file
+	_load_config()
+	print("Test successful!")
+
+def _load_config():
+	if os.path.exists('config.ini'):
+		config_parser = configparser.ConfigParser()
+		config_parser.read('config.ini')
+		if config_parser.has_option('Twitch', 'key') and len(config_parser.get('Twitch', 'key')) > 0:
+			return config_parser
+		else:
+			exit_missing_credentials()
+	else:
+		with open('config.ini','w') as f:
+			config_parser = configparser.ConfigParser(allow_no_value=True)
+			config_parser.add_section('Twitch')
+			config_parser.set('Twitch', '; General Twitch settings here')
+			config_parser.set('Twitch', 'key')
+			config_parser.set('Twitch', 'min_delay', '60')
+			config_parser.write(f)
+		exit_missing_credentials()
+
+def exit_missing_credentials():
+	print('>> Please enter the credentials to the config.ini first!')
+	exit()
+
+if __name__ == '__main__':
+	# main!
+	main()
